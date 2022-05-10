f234a1d839b321e50f289c054f315a4c9896f9a6
choiyurim01 <me_28@huray.net>
2022-05-10 16:08:31 +0900

    검색창까지 구현

--- js/common.js
+++ js/common.js
@@ -13,4 +13,23 @@ function subStage() {
     $('.sub-stage').slideToggle()
   });
 
+  //검색창 호버시
+  $('.navi-container .search-ipt').on('mouseover', function(){
+    $('.navi-container .navi-wrapper').addClass('d-none')
+    $('.navi-container .sub-stage').css('height','100vh')
+    $('.sub-wrapper .sub-wrap').addClass('d-none')
+    $('.navi-container .search-wrapper').addClass('active')
+    $('.search-wrapper btn-close').addClass('hidden')
+    $('.navi-container .quickLink-wrap').addClass('d-block')
+  });
+
+  //취소버튼 클릭시
+  $('.search-wrapper .btn-close').on('click', function(){
+    $('.navi-container .navi-wrapper').removeClass('d-none')
+    $('.navi-container .sub-stage').css('height','calc(100vh - 40px)')
+    $('.sub-wrapper .sub-wrap').removeClass('d-none')
+    $('.navi-container .search-wrapper').removeClass('active')
+    $('.search-wrapper btn-close').removeClass('hidden')
+    $('.navi-container .quickLink-wrap').removeClass('d-block')
+  });
 }
